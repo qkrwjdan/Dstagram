9ef7a3e90e88b1ba9f2042dcd39bdf3a04eb05e7
qkrwjdan <madogisa12@naver.com>
2019-05-27 21:47:21 +0900

    accounts앱 추가

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('photo.urls')),
 ]
+
+#static 을 사용한 MEDIA_URL에 해당하는 주소를 가진 요청에 대해서 MEDIA_ROOT에서 찾아서 응답하도록 한다.
+urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
